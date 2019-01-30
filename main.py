73c3c4d203ed997ea8f9d2ebfda56da084a03566
Michael Stefferson <mstefferson@gmail.com>
2019-01-30 13:44:59 -0800

    Added inner function

--- main.py
+++ main.py
@@ -4,15 +4,27 @@ import my_module
 import util
 
 
+def inner_fnc():
+    logger = logging.getLogger(__name__)
+    logger.info('Inner fnc')
+    logger.error('Inner fnc')
+
+
 def main():
     # logger = logging.getLogger()
     util.setup_logging()
+    # logger = logging.getLogger(__name__)
     # send some messages
+    # logger.debug('debug message')
+    # logger.info('info message')
     logging.debug('debug message')
     logging.info('info message')
     logging.warning('warn message')
     logging.error('error message')
     logging.critical('critical message')
+
+    # inner fnc
+    inner_fnc()
 
     # run func1
     lib1.func1()
